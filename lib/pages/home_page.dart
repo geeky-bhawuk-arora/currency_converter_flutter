133a54dddc9ad2be349ca65599c9704febb820b4
Bhawuk Arora <bhawukarora042@gmail.com>
2025-11-05 11:40:40 +0530

    ui

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -6,22 +6,40 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.lightGreenAccent,
+      backgroundColor: Colors.blue,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(
-                '42',
+                '0',
                 style: TextStyle(
-                  fontSize: 24,
+                  fontSize: 40,
                   fontWeight: FontWeight.bold,
                   color: Color.fromARGB(255, 5, 0, 7),
                 ),
             ),
             TextField(
               style: TextStyle(
-                color: Colors.blue,
+                color: Colors.white,
+              ),
+              decoration: InputDecoration(
+                hintText: "Please enter the currency in INR !!",
+                hintStyle: TextStyle(
+                  color: Colors.black,
+                ),
+                prefixIcon: Icon(Icons.monetization_on),
+                prefixIconColor: Colors.black,
+                filled: true,
+                fillColor: Colors.white,
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.red,
+                    width: 2.0,
+                    style: BorderStyle.solid,
+                    strokeAlign: BorderSide.strokeAlignInside
+                  )
+                )
               ),
             ),
           ],
